23cb12f4006fa06913fc220c914ce21b70d18c7a
Yaroslav Tsvyk <yaroslav.tsvyk23@gmail.com>
2025-08-05 21:52:48 +0300

    Deleted Author and refactored Book

--- src/main/java/com/example/bookify/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookify.repository;
-
-import com.example.bookify.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
